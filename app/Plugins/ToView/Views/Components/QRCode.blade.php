e2a64ca03043b5701e7e79454c8a3878bc3a9df8
Linn Benson <bc@tobc.me>
2026-08-29 04:51:57 +0800

    移除 LaravelTool 和 ToView 插件的版本跟踪

--- app/Plugins/ToView/Views/Components/QRCode.blade.php
+++
@@ -1,5 +0,0 @@
-@props([ 'size' => 128, 'config' => [] ])
-@php
-    $rid = 'QRCode-'.uniqid(); // 唯一标识符
-@endphp
-<span id="{{ $rid }}"></span><script>Core.QRCode( $( 'span#{{$rid}}' ), `{{$slot}}`, {{is_numeric( $size ) ? $size : 128}}, @json( $config ) );</script>
